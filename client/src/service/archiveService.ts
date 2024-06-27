f0f8183639f9cc8bda2de76b28fb45789f701e22
Álex Fernando <alex.raskav@gmail.com>
2024-06-26 21:41:42 -0300

    feat: archive service on frontend and a base settings for baseAPI and default requesterror message

--- client/src/service/archiveService.ts
+++ client/src/service/archiveService.ts
@@ -1,13 +1,86 @@
-import axios from "axios"
+import axios from "axios";
+import { BASE_API, defaultErrorRequestMessage } from "./settings";
 
-const BASE_API = process.env.REACT_APP_BASEAPI_URL;
-
-function getAllArchivesByFolderId(folderId: string) {
-    axios.get(`${BASE_API}/api/archives/folder/${folderId}`).then((response) => {
-        console.log(response.data)
-    }).catch((error) => {
-        console.log("Aconteceu um erro enquanto fazia requisição: " + error)
-    })
+interface ArchiveProps {
+  name: string;
+  folderId: string;
+  userId: string;
+  content?: string;
 }
 
-export {getAllArchivesByFolderId}
+function createArchive({ name, folderId, userId, content }: ArchiveProps) {
+  const newArchive = {
+    name,
+    folderId,
+    userId,
+    content,
+  };
+
+  axios
+    .post(`${BASE_API}/api/archives/`, newArchive)
+    .then((response) => {
+      return response.data;
+    })
+    .catch((error) => {
+      console.log(defaultErrorRequestMessage + error);
+    });
+}
+
+function updateArchive(
+  id: string,
+  { name, content }: { name?: string; content?: string }
+) {
+  axios
+    .put(`${BASE_API}/api/archives/${id}`, {
+      name,
+      content,
+    })
+    .then((response) => {
+      return response.data;
+    })
+    .catch((error) => {
+      console.log(defaultErrorRequestMessage + error);
+    });
+}
+
+function deleteArchive(id: string) {
+  axios
+    .delete(`${BASE_API}/api/archives/${id}`)
+    .then((response) => {
+      console.log(response.data);
+    })
+    .catch((error) => {
+      console.log(defaultErrorRequestMessage + error);
+    });
+}
+
+function getAllArchivesByFolderId(folderId: string) {
+  axios
+    .get(`${BASE_API}/api/archives/folder/${folderId}`)
+    .then((response) => {
+      console.log(response.data);
+    })
+    .catch((error) => {
+      console.log(defaultErrorRequestMessage + error);
+    });
+}
+
+function getSourceFolderByUserId(userId: string) {
+  axios
+    .get(`${BASE_API}/api/archives/folder/source/${userId}`)
+    .then((response) => {
+      console.log(response.data);
+      return response.data;
+    })
+    .catch((error) => {
+      console.log(defaultErrorRequestMessage + error);
+    });
+}
+
+export {
+  getAllArchivesByFolderId,
+  getSourceFolderByUserId,
+  createArchive,
+  deleteArchive,
+  updateArchive,
+};
---
+++ client/src/service/settings.ts
@@ -0,0 +1,3 @@
+const BASE_API = process.env.REACT_APP_BASEAPI_URL;
+const defaultErrorRequestMessage = "Ocurred a error on the request: ";
+export { BASE_API, defaultErrorRequestMessage };
